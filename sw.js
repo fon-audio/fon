8fa21a0a0fdff8c8952b1237fa0ee41d898f3b2e
Konstantin <xxtech2@gmail.com>
2026-09-18 15:34:55 +0300

    Update from main project

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    это чужие потоки, на них нужен интернет.
    ============================================================ */
 
-const APP_VERSION = '1.0.28';
+const APP_VERSION = '1.0.29';
 const CACHE = 'fon-ui-' + APP_VERSION;
 
 const SHELL = [
